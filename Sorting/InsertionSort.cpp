873b24c9a73827b8ac7f0dfe7c093f2dfbcdf477
shljsl75891 <56100355+shljsl75891@users.noreply.github.com>
2022-07-11 10:46:12 +0530

    Add files via upload

---
+++ Sorting/InsertionSort.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+void insertionSort(int *arr, int size) {
+        for(int i = 1; i < size; i++) {
+                int temp = arr[i];
+                int j = i - 1;
+                while(j >= 0 && arr[j] > temp) {
+                        arr[j + 1] = arr[j];
+                        j = j - 1;
+                } arr[j + 1] = temp;
+        }
+}
+
+int main() {
+        int n; cin >> n;
+        int *arr = new int[n];
+        for(int i = 0; i < n; i++) {
+                cin >> arr[i];
+        } insertionSort(arr, n);
+        for(int i = 0; i < n; i++) {
+                cout << arr[i] << " ";
+        } delete[] arr;
+}
