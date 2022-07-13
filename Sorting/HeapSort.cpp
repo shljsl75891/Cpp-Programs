3d91fc88bd52fb536344a5da811c6caa05a53ec6
shljsl75891 <shljsl75891@gmail.com>
2022-07-13 16:40:19 +0530

    Better HeapSort

--- Sorting/HeapSort.cpp
+++ Sorting/HeapSort.cpp
@@ -1,62 +1,37 @@
 #include <iostream>
 using namespace std;
 
-void insertInHeap(int *arr, int n) {
-	for(int index = 1; index < n; index++) {
-		int temp = arr[index];
-		int parentIndex = (index - 1)/2;
-		while(temp > arr[parentIndex]) {
-			arr[index] = arr[parentIndex];
-			index = parentIndex;
-			if(index == 0) {
-				break;
-			}
-			parentIndex = (index - 1)/2;
-		} arr[index] = temp;
-	}
+void heapify(int *arr, int size, int index) {
+    int largest = index;
+    int left = 2 * index + 1, right = left + 1;
+    if(left < size && arr[largest] < arr[left]) {
+        largest = left;
+    } if(right < size && arr[largest] < arr[right]) {
+        largest = right;
+    } if(largest != index) {
+        swap(arr[largest], arr[index]);
+        heapify(arr, size, largest);
+    }
 }
 
-void deleteAndSort(int *arr, int &n) {
-	int index = 0, leftIndex, rightIndex;
-	int item = arr[0], last = arr[n - 1];
-	leftIndex = 2 * index + 1;
-	rightIndex = leftIndex + 1; 
-	n--;
-	while(leftIndex < n) {
-		int childIndex = arr[leftIndex] > arr[rightIndex] ? leftIndex : rightIndex;
-		if(last > arr[childIndex]) {
-			arr[index] = last; 
-			break;
-		}	
-		else {
-			arr[index] = arr[childIndex];
-			index = childIndex;
-		}
-		leftIndex = 2 * index + 1;
-		rightIndex = leftIndex + 1; 
-	} arr[index] = last;
-	arr[n] = item;
-}
-
-void HeapSort(int *arr, int n) {
-	insertInHeap(arr, n);
-	while(n > 0) {
-		deleteAndSort(arr, n);
-	}
+void HeapSort(int *arr, int size) {
+    for(int i = size/2 - 1; i >= 0; i--) {
+        heapify(arr, size, i);
+    } while(size > 0) {
+        swap(arr[size - 1],arr[0]);
+        size--;
+        heapify(arr, size, 0);
+    } 
 }
 
 int main() {
-	int n; cin >> n;
-	int *arr = new int[n];
-	for(int i = 0; i < n; i++) {
-		cin >> arr[i];
-	} 	
-	for(int i = 0; i < n; i++) {
-		cout << arr[i] << " ";
-	} cout << "\n";
-	HeapSort(arr, n);
-	for(int i = 0; i < n; i++) {
-		cout << arr[i] << " ";
-	} cout << "\n";		
-	delete[] arr;
+    int n; cin >> n;
+    int *arr = new int[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    } HeapSort(arr, n);
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    delete[] arr;
 }
