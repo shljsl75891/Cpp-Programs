50d9eba6696422ec1018a1e3329ec9424ef7a888
shljsl75891 <shljsl75891@gmail.com>
2022-07-15 11:37:11 +0530

    print weighted graph

--- Graphs/GraphRepresentation.cpp
+++ Graphs/GraphRepresentation.cpp
@@ -1,35 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int N = 1e3;
+vector<pair<int, int> > graph[N];
+
+void createGraph(int vertices, int edges) {
+    for(int i = 0; i < edges; i++) {
+        int v1, v2, wt;
+        cin >> v1 >> v2 >> wt;
+        graph[v1 - 1].push_back(make_pair(v2, wt));
+        graph[v2 - 1].push_back(make_pair(v1, wt));
+    } 
+}
+
+void printGraph(int vertices) {
+    for(int i = 0; i < vertices; i++) {
+        cout << i + 1 << ": ";
+        for(pair<int, int> p : graph[i]) {
+            cout << "(" << p.first << " " << p.second << ") ";
+        } cout << endl;
+    }
+}
 
 int main() {
     int vertices, edges; 
     cin >> vertices >> edges;
-    int graph1[vertices][vertices];
-    vector<pair<int, int> > graph2[vertices];
-    for(int i = 0; i < vertices; i++) {
-        for(int j = 0; j < vertices; j++) {
-            graph1[i][j] = 0;
-        } 
-    }
-    for(int i = 0; i < edges; i++) {
-        int v1, v2, wt;
-        cin >> v1 >> v2 >> wt;
-        graph1[v1 - 1][v2 - 1] = wt;
-        graph1[v2 - 1][v1 - 1] = wt;
-        graph2[v1 - 1].push_back(make_pair(v2, wt));
-        graph2[v2 - 1].push_back(make_pair(v1, wt));
-    } 
-    cout << "Adjacency Matrix: " << endl;
-    for(int i = 0; i < vertices; i++) {
-        for(int j = 0; j < vertices; j++) {
-            cout << graph1[i][j] << " ";
-        } cout << endl;
-    }
-    cout << "\nAdjacency List: " << endl;
-    for(int i = 0; i < vertices; i++) {
-        cout << i + 1 << ": ";
-        for(pair<int, int> p : graph2[i]) {
-            cout << "(" << p.first << " " << p.second << ") ";
-        } cout << endl;
-    }
+    createGraph(vertices, edges);
+    printGraph(vertices);
 }
